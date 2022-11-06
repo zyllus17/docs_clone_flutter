4892d1b9b86fd49d002494d8c0f7aa2668fe0b45
zyllus17 <mrfhassan17@gmail.com>
2022-11-06 12:47:40 +0530

    Added GoogleSignIn

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,14 +1,21 @@
 import 'package:docs_clone_flutter/colors.dart';
+import 'package:docs_clone_flutter/repository/auth_repository.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class LoginScreen extends StatelessWidget {
+class LoginScreen extends ConsumerWidget {
   const LoginScreen({Key? key}) : super(key: key);
+
+  void signInWithGoogle(WidgetRef ref) {
+    ref.read(authRepositoryProvider).signInWithGoogle();
+  }
+
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return Scaffold(
       body: Center(
         child: ElevatedButton.icon(
-          onPressed: () {},
+          onPressed: () => signInWithGoogle(ref),
           icon: Image.asset(
             'assets/images/g-logo-2.png',
             height: 20,
