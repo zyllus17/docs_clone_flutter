a2198a580bef04cdb075faf7c9df31f120b7d1b6
zyllus17 <mrfhassan17@gmail.com>
2022-11-22 21:17:08 +0530

    Generate token logic

--- lib/repository/auth_repository.dart
+++ lib/repository/auth_repository.dart
@@ -1,29 +1,66 @@
-// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'dart:convert';
+
+import 'package:docs_clone_flutter/constants.dart';
+import 'package:docs_clone_flutter/models/error_model.dart';
+import 'package:docs_clone_flutter/models/user_model.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:google_sign_in/google_sign_in.dart';
+import 'package:http/http.dart';
 
 final authRepositoryProvider = Provider(
   (ref) => AuthRepository(
     googleSignIn: GoogleSignIn(),
+    client: Client(),
   ),
 );
 
+final userProvider = StateProvider<UserModel?>((ref) => null);
+
 class AuthRepository {
   final GoogleSignIn _googleSignIn;
+  final Client _client;
   AuthRepository({
     required GoogleSignIn googleSignIn,
-  }) : _googleSignIn = googleSignIn;
+    required Client client,
+  })  : _googleSignIn = googleSignIn,
+        _client = client;
 
-  void signInWithGoogle() async {
+  Future<ErrorModel> signInWithGoogle() async {
+    ErrorModel error = ErrorModel(
+      error: 'Some unexpected error occured',
+      data: null,
+    );
     try {
       final user = await _googleSignIn.signIn();
+
       if (user != null) {
-        print(user.email);
-        print(user.displayName);
-        print(user.photoUrl);
+        final userAcc = UserModel(
+          email: user.email,
+          name: user.displayName!,
+          profilePic: user.photoUrl!,
+          uid: '',
+          token: '',
+        );
+        var res = await _client.post(
+          Uri.parse('$host/api/signup'),
+          body: userAcc.toJson(),
+          headers: {
+            'Content-Type': 'application/json; charset=UTF-8',
+          },
+        );
+        switch (res.statusCode) {
+          case 200:
+            final newUser = userAcc.copyWith(
+              uid: jsonDecode(res.body)['user']['_id'],
+              token: jsonDecode(res.body)['token'],
+            );
+            error = ErrorModel(error: null, data: newUser);
+            break;
+        }
       }
     } catch (e) {
-      print(e);
+      error = ErrorModel(error: e.toString(), data: null);
     }
+    return error;
   }
 }
