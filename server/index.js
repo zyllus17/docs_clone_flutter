8dc4cca7b57ef909c1729f095b259fc208e44c65
zyllus17 <mrfhassan17@gmail.com>
2022-11-22 21:17:58 +0530

    Added mongoDB server

--- server/index.js
+++ server/index.js
@@ -1,9 +1,23 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors");
+const authRouter = require("./routes/auth");
 
 const PORT = process.env.PORT | 3001;
 
 const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(authRouter);
+
+const DB = "mongodb+srv://marufhassan:mongodb@cluster0.stoqstw.mongodb.net/?retryWrites=true&w=majority";
+
+mongoose.connect(DB).then(() => {
+    console.log("Connection successful");
+}).catch((err) => {
+    console.log(err)
+});
 
 app.listen(PORT, "0.0.0.0", () => {
     console.log(`connected at port ${PORT}`);
